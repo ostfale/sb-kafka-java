3d9d4242550691dc4ad4b13321bdbad5c2ee17b8
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2020-10-09 08:17:11 +0200

    Initial commit

---
+++ src/main/java/de/ostfale/kafka/sbkafkajava/SbKafkaJavaApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.kafka.sbkafkajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKafkaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/kafka/sbkafkajava/SbKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.kafka.sbkafkajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
